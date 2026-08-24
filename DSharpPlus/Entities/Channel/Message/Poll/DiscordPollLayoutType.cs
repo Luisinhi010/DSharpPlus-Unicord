be17f2d65504402a92aa15b9d3392611c66d8e16
Luisinho01 <91190561+Luisinhi010@users.noreply.github.com>
2026-08-24 14:53:03 -0300

    feat: backport poll models from upstream (ChatGPT)

---
+++ DSharpPlus/Entities/Channel/Message/Poll/DiscordPollLayoutType.cs
@@ -0,0 +1,10 @@
+namespace DSharpPlus.Entities
+{
+    /// <summary>
+    /// Represents Discord's poll layout type.
+    /// </summary>
+    public enum DiscordPollLayoutType : int
+    {
+        Default = 1
+    }
+}
